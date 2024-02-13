c7cdd1a38f4cf7e1cf7a30f4bb42590bbfcbc19a
usmanx1 <119616036+usmanx1@users.noreply.github.com>
2024-02-13 14:42:28 +0500

    Created a sample Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Flashcard from "./components/atoms/Flashcard/Flashcard";
+import { Navbar } from "./components/molecules";
 import "./App.css";
 
 const App: React.FC = () => {
     return (
+        <>
+        <Navbar/>
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-4xl font-bold mt-3 cursor-pointer hover:text-blue-500">
                 Flashcards
             </h1>
             <Flashcard />
         </div>
+        </>
     );
 };
 
