09b0257366d760549075c0fb4c750b9a78ad737b
usmanx1 <119616036+usmanx1@users.noreply.github.com>
2024-02-14 17:22:35 +0500

    Creates Signup and Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Flashcard from "./components/atoms/Flashcard/Flashcard";
-import { Navbar } from "./components/molecules";
+import { Navbar, Signup, Login } from "./components/molecules";
 import "./App.css";
 
 const App: React.FC = () => {
     return (
-        <>
-        <Navbar/>
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mt-3 cursor-pointer hover:text-blue-500">
-                Flashcards
-            </h1>
-            <Flashcard />
-        </div>
-        </>
+        <Router>
+            <>
+                <Navbar />
+                <div className="flex flex-col items-center justify-center ">
+                    <h1 className="text-4xl font-bold mt-3 cursor-pointer hover:text-blue-500">
+                        Flashcards
+                    </h1>
+                    </div>
+                    <Routes>
+                        <Route path="/" element={<Flashcard />} />
+                    </Routes>
+
+                <Routes>
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </>
+        </Router>
     );
 };
 
