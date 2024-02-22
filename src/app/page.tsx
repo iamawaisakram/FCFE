8c3f37db10f62ce989235a91ff1434030f71566d
usmanx1 <119616036+usmanx1@users.noreply.github.com>
2024-02-22 15:39:08 +0500

    Implements user authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 // app/page.tsx
 import React from 'react';
-import { Flashcard } from '@/components/atoms';
-import { Navbar } from '@/components/molecules';
+import HomePage from './home/page';
+
 
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Navbar/>
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mt-3 cursor-pointer hover:text-blue-500">
-          Flashcards
-        </h1>
-      </div>
-      <Flashcard />
+      <HomePage/>
     </div>
   );
 };
